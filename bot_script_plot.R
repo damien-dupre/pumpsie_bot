a0fdbb5d93cabaa37fdcc81ac01511b38f33fec9
“damien-dupre” <damien.dupre.ac@gmail.com>
2022-04-07 11:40:44 +0100

    filter outliers

--- bot_script_plot.R
+++ bot_script_plot.R
@@ -92,7 +92,8 @@ df <- pumps_county_df |>
   mutate(url = glue("https://pumps.ie/api/getStationsByPriceAPI.php?county={county}&minLat={minLat}&maxLat={maxLat}&minLng={minLng}&maxLng={maxLng}&fuel={fuel}")) |> 
   group_by(province, county, fuel) |> 
   summarise(api_call(url)) |> 
-  filter(price < 500 & dateupdated > (Sys.Date() - 14) & dateupdated < Sys.Date()) |> 
+  filter(dateupdated > (Sys.Date() - 14) & dateupdated < Sys.Date()) |>
+  filter(price < 500 & price > 130) |> 
   distinct(ID, dateupdated, fuel, .keep_all = TRUE)
 
 province_title <- unique(df$province)[1]
