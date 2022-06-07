f7e77b40983a649d3579025de1618bb7a0783dd1
“damien-dupre” <damien.dupre.ac@gmail.com>
2022-06-07 15:00:34 +0100

    script table

--- bot_script_table.R
+++ bot_script_table.R
@@ -1,5 +1,7 @@
 # libraries --------------------------------------------------------------------
+library(fontawesome)
 library(glue)
+library(gt)
 library(here)
 library(rtweet)
 library(scales)
@@ -53,9 +55,7 @@ df <-
   ) |> 
   rowwise() |> 
   filter(between(price, c1, c99)) |> 
-  ungroup()
-
-test <- df |> 
+  ungroup() |> 
   mutate(dateupdated = if_else(dateupdated == Sys.Date() - 1, "yesterday", "daybefore")) |> 
   group_by(County, fuel, dateupdated) |> 
   summarise(price_avg = mean(price, na.rm = TRUE)) |> 
@@ -63,5 +63,75 @@ test <- df |>
   drop_na(yesterday) |> 
   mutate(perc_change = ((yesterday - daybefore)/daybefore)) |> 
   left_join(df |> filter(dateupdated == Sys.Date() - 1) |> count(County, fuel)) |> 
-  left_join(df |> filter(dateupdated == Sys.Date() - 1) |> group_by(County, fuel) |> slice_min(price) |> select(County, fuel, price_min = price, name, Zone)) 
-library(gt)
+  left_join(df |> filter(dateupdated == Sys.Date() - 1) |> group_by(County, fuel) |> slice_min(price, with_ties = FALSE) |> select(County, fuel, price_min = price, name, Zone)) |> 
+  ungroup()
+
+# visualisation ----------------------------------------------------------------
+df |>
+  arrange(fuel) |>
+  mutate(
+    `Avg Price` = yesterday/100,
+    `Min Price` = price_min/100,
+  ) |>
+  select(Fuel = fuel, County, N = n, `Avg Price`, Arrow = perc_change, Change = perc_change, Location = name, Zone, `Min Price`) |>
+  gt(groupname_col = "Fuel") |>
+  sub_missing() |>
+  opt_row_striping() |> 
+  cols_label(Arrow = "", Change = "") |>
+  tab_header(
+    title = md("**Fuel prices reported on *pumps.ie* yesterday by Irish county**"),
+    subtitle = glue("updated: {Sys.Date()}")
+  ) |>
+  tab_spanner(
+    label = "Lowest Price Reported",
+    columns = c(`Min Price`, Location, Zone)
+  ) |>
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_labels(columns = everything())
+  ) |>
+  tab_style(
+    style = cell_text(size = px(12)),
+    locations = cells_body(columns = `Min Price`)
+  ) |> 
+  fmt_currency(columns = c(`Avg Price`, `Min Price`), currency = "EUR") |>
+  fmt_percent(columns = Change, decimals = 1) |> 
+  cols_merge(
+    columns = c(Location, Zone),
+    pattern = "<div><span style='font-weight:bold;font-variant:small-caps;font-size:12px'>{1}</div>
+        <div><span style ='font-weight:bold;color:grey;font-size:10px'>{2}</span></div>"
+  ) |> 
+  text_transform(
+    locations = cells_body(columns = County),
+    fn = function(x){glue("<div><span style='font-weight:bold;font-variant:small-caps;font-size:14px'>{x}</div>")}
+  ) |>
+  text_transform(
+    locations = cells_body(columns = Arrow),
+    fn = function(x){
+      case_when(
+      x < 0 ~ as.character(fa("arrow-down", fill = "red")),
+      x > 0 ~ as.character(fa("arrow-up", fill = "green")),
+      x == 0 ~ as.character(fa("equals", fill = "grey"))
+    )}
+  ) |>
+  text_transform(
+    locations = cells_body(columns = Arrow, rows = is.na(Arrow)),
+    fn = function(x){"—"}
+  ) |> 
+  tab_footnote(
+    footnote = md("Missing counties among the 26 (IE) have no fuel price reported on *pumps.ie* yesterday.")
+  ) |> 
+  tab_footnote(
+    footnote = "No data available the day before to compare with.",
+    locations = cells_body(columns = Arrow, rows = is.na(Arrow))
+  ) |> 
+  tab_source_note(
+    source_note = "Source: pumps.ie/@damien_dupre"
+  ) |> 
+  gtsave("pumps.png")
+
+# tweet ------------------------------------------------------------------------
+post_tweet(glue("Average fuel prices reported on pumps.ie yesterday. #Ireland #Petrol #Diesel #FuelPrice"), media = "pumps.png", token = bot_token)
+
+# options out ------------------------------------------------------------------
+unlink("pumps.png")
